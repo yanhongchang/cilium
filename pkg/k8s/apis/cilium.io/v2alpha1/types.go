3b38d0be2a3b80bb7553e4b3e7b79dd685354b27
Gilberto Bertin <jibi@cilium.io>
2022-02-07 15:14:12 +0100

    k8s: add egressGateway property to CiliumEgressNATPolicy
    This commit adds a new egressGateway property to the
    CiliumEgressNATPolicy object, which improves the way the egress gateway
    node is selected and configured.
    
    Before this change a CiliumEgressNATPolicy would only allow to specify a
    an egressSourceIP, which was used both as:
    
    * IP to masquerade traffic leaving the cluster
    * IP to identify which node was acting as gateway for the policy
    
    Now, with the egressGateway property it's possible to explicitly
    identify which node should act as gateway by using a nodeSelector based
    on node labels. In this way, workers node will forward traffic to the
    egress gateway node by using its internal node IP.
    
    Moreover, the new property extends the way the egress IP can be
    configured, as a user can either:
    
    * specify the egress IP that should be used to masquerade traffic
    * specify the interface that should be used to masquerade traffic: in
      this case the first IPv4 assigned to that interface will be used as
      egress IP
    * not specify anything: in this case the first IPv4 assigned to the
      interface for the default route will be used
    
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- pkg/k8s/apis/cilium.io/v2alpha1/types.go
+++ pkg/k8s/apis/cilium.io/v2alpha1/types.go
@@ -62,7 +62,49 @@ type CiliumEgressNATPolicySpec struct {
 	// redirected to node with ip 192.168.1.100 and SNAT’ed with IP address 192.168.1.100.
 	//
 	// +kubebuilder:validation:Pattern=`((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))`
+	// +kubebuilder:validation:OneOf
 	EgressSourceIP string `json:"egressSourceIP"`
+
+	// EgressGateway is the gateway node responsible for SNATing traffic.
+	//
+	// +kubebuilder:validation:OneOf
+	EgressGateway *EgressGateway `json:"egressGateway"`
+}
+
+// EgressGateway identifies the node that should act as egress gateway for a
+// given egress NAT policy. In addition to that it also specifies the
+// configuration of said node (which egress IP or network interface should be
+// used to SNAT traffic).
+type EgressGateway struct {
+	// This is a label selector which selects the node that should act as
+	// egress gateway for the given policy.
+	// In case multiple nodes are selected, only the first one in the
+	// lexicograph ordering will be used.
+	// This field follows standard label selector semantics.
+	//
+	// +kubebuilder:validation:Required
+	NodeSelector *slimv1.LabelSelector `json:"nodeSelector"`
+
+	// Interface is the network interface to which the egress IP is assigned.
+	//
+	// When none of the Interface or EgressIP fields is specified, the
+	// policy will use the first IPv4 assigned to the interface with the
+	// default route.
+	Interface string `json:"interface,omitempty"`
+
+	// EgressIP is a source IP address that the egress traffic is redirected
+	// to and SNATed with.
+	//
+	// Example:
+	// When it is set to "192.168.1.100", matched egress packets will be
+	// redirected to node with IP 192.168.1.100 and SNAT’ed with IP address 192.168.1.100.
+	//
+	// When none of the Interface or EgressIP fields is specified, the
+	// policy will use the first IPv4 assigned to the interface with the
+	// default route.
+	//
+	// +kubebuilder:validation:Pattern=`((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))`
+	EgressIP string `json:"egressIP,omitempty"`
 }
 
 type EgressRule struct {
